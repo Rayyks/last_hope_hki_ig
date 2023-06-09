958e2bc28dcee07d76aaaf09b7ba323e8f451a6a
Rayyks <rayydna14@gmail.com>
2023-06-09 14:23:43 +0700

    backup
    Signed-off-by: Rayyks <rayydna14@gmail.com>

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+<!-- =========================================================
+* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
+==============================================================
+
+* Product Page: https://themeselection.com/products/sneat-bootstrap-html-admin-template/
+* Created by: ThemeSelection
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright ThemeSelection (https://themeselection.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html
+    lang="en"
+    class="light-style customizer-hide"
+    dir="ltr"
+    data-theme="theme-default"
+    data-assets-path="{{ asset('public/sneat/') }}"
+    data-template="vertical-menu-template-free"
+>
+<head>
+    <meta charset="utf-8"/>
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>{{ __('menu.auth.login') }} | {{ config('app.name') }}</title>
+
+    <meta name="description" content=""/>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{asset('sneat/img/favicon/favicon.ico')}}"/>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet"
+    />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="{{asset('sneat/vendor/fonts/boxicons.css')}}"/>
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" class="template-customizer-core-css" href="{{asset('sneat/vendor/css/core.css')}}"/>
+    <link rel="stylesheet" class="template-customizer-theme-css"
+          href="{{asset('sneat/vendor/css/theme-default.css')}}"/>
+    <link rel="stylesheet" href="{{asset('sneat/css/demo.css')}}"/>
+
+    <!-- Page -->
+    <link rel="stylesheet" href="{{asset('sneat/vendor/css/pages/page-auth.css')}}"/>
+</head>
+
+<body>
+<!-- Content -->
+
+<div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+            <!-- Register -->
+            <div class="card">
+                <div class="card-body">
+                    <!-- Logo -->
+                    <div>
+                        <a href="/index" >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="36" fill="currentColor" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+                            <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z"/>
+                            </svg>
+                        </a>
+                    </div>
+                    <h1 class="text-center text-dark">Reset Password</h1>
+                    <div class="app-brand justify-content-center">
+                        <a href="{{ route('home') }}" class="app-brand-link gap-2">
+                            <img src="https://www.polibatam.ac.id/wp-content/uploads/2021/09/logo.png" alt="{{ config('app.name') }}" srcset="" width="75px">
+                        </a>
+                    </div>
+                    <div>
+                        @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
+                        @if (session()->has('status'))
+                        <div class="alert alert-success">
+                            {{ session()->get('status') }}
+                        </div>
+                        @endif
+                    </div>
+                    <h3 class="text-center">Lupa password?</h3>
+                    <p class="text-center">Tolong masukan email atau password reset request</p>
+                    <form id="formAuthentication" class="mb-3" action="{{route('password.update')}}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="password" class="form-label">password</label>
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password_confirmation" class="form-label">Konfirmasi Password</label>
+                            <input type="password_confirmation" name="password_confirmation" class="form-control">
+                        </div>
+                            <input type="password_confirmation"  value="Update Password" class="btn btn-primary d-grid w-100">
+                    </form>
+                </div>
+            </div>
+            <!-- /forgot -->
+        </div>
+    </div>
+</div>
+
+<!-- / Content -->
+</body>
+</html>
